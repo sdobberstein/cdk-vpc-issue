40a269094d7c4a8e5b7a92a5e1695ea521834793
Sean Dobberstein <sean.dobberstein@amway.com>
2021-11-19 15:37:58 -0500

    moving stack into bin file

--- bin/cdk-vpc-issue.ts
+++ bin/cdk-vpc-issue.ts
@@ -1,14 +1,42 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkVpcIssueStack } from '../lib/cdk-vpc-issue-stack';
+import {Construct, Stack, StackProps} from "@aws-cdk/core";
+import {InterfaceVpcEndpointAwsService, SubnetFilter, Vpc} from "@aws-cdk/aws-ec2";
 
 const app = new cdk.App();
-new CdkVpcIssueStack(app, 'CdkVpcIssueStack', {
+
+interface MyStackProps extends StackProps {
+  readonly subnetIds: string[];
+  readonly vpcId: string;
+}
+
+class MyStack extends Stack {
+  constructor(scope: Construct, id: string, props: MyStackProps) {
+    super(scope, id, props);
+
+    const vpc = Vpc.fromLookup(this, 'Vpc', {
+      vpcId: props.vpcId,
+    });
+
+    const secretsManagerEndpoint = vpc.addInterfaceEndpoint('SecretsManagerEndpoint', {
+      service: InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
+      subnets: {
+        subnetFilters: [SubnetFilter.byIds(props.subnetIds)],
+      },
+    });
+    secretsManagerEndpoint.connections.allowDefaultPortFromAnyIpv4();
+  }
+}
+
+new MyStack(app, 'MyStack', {
   env: {
-    account: '',
-    region: '',
+    account: 'xxx',
+    region: 'us-east-1',
   },
-  subnetIds: [''],
-  vpcId: '',
+  subnetIds: [
+    'xxx',
+    'xxx'
+  ],
+  vpcId: 'xxx'
 });
